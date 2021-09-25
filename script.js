fbd70d91ce435cf90d30e567dcdf2c201971ae86
Angus Cheng <anguscheng389@gmail.com>
2021-09-26 03:49:10 +1000

    Added checkMatchRow() which checks for any 3 in a row matches

--- script.js
+++ script.js
@@ -1,53 +1,101 @@
 const board = Array.from(document.querySelectorAll('.gameBoard > *'));
 
 const gameBoard = (() => {
-    let board = [[]];
+    let board = new Array(3);
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(3);
+    }
 
-    const checkAvail = (x, y) => {
-        if (board[x][y] != null) {
+    let turn = 0;
+
+    const checkAvail = (x) => {
+        if (x == 3) {
             return false;
-        };
+        }
 
         return true;
     };
 
+    const addSym = (sym, x, y) => {
+        if (checkAvail(x, y)) {
+            board[y][x] = sym;
+        }
+        checkMatchRow();
+
+        return;
+    };
+
+    const checkMatchRow = () => {
+        rowSum = '';
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                rowSum += board[i][j];
+            }
+        }
+
+        if (rowSum.search('XXX') != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const makeTurn = () => {
+        let sym;
+        if (turn % 2 == 0) {
+            sym = 'X';
+        } else {
+            sym = 'O';
+        }
+        turn++;
+
+        return sym;
+    };
+
     return {
         board,
-        checkAvail
+        addSym,
+        makeTurn 
     };
 })();
 
 const displayController = (() => {
-    const changeDisplay = (cell) => {
+    const changeDisplay = (cell, playerTurn) => {
         const cellName = cell.target.className;
+        console.log(cell.target.textContent);
+
+        if (cell.target.textContent != '') {
+            return [3, 3];
+        }
+
         switch(cellName) {
             case 'topLeft':
-                board[0].textContent = 'f';
-                break;
+                board[0].textContent = playerTurn;
+                return [0, 0];
             case 'topMiddle':
-                board[1].textContent = 'f';
-                break;
+                board[1].textContent = playerTurn;
+                return [1, 0];
             case 'topRight':
-                board[2].textContent = 'f';
-                break;
+                board[2].textContent = playerTurn;
+                return [2, 0];
             case 'centreLeft':
-                board[3].textContent = 'f';
-                break;
+                board[3].textContent = playerTurn;
+                return [0, 1];
             case 'centreMiddle':
-                board[4].textContent = 'f';
-                break;
+                board[4].textContent = playerTurn;
+                return [1, 1];
             case 'centreRight':
-                board[5].textContent = 'f';
-                break;
+                board[5].textContent = playerTurn;
+                return [2, 1];
             case 'bottomLeft':
-                board[6].textContent = 'f';
-                break;
+                board[6].textContent = playerTurn;
+                return [0, 2];
             case 'bottomMiddle':
-                board[7].textContent = 'f';
-                break;
+                board[7].textContent = playerTurn;
+                return [1, 2];
             case 'bottomRight':
-                board[8].textContent = 'f';
-                break;
+                board[8].textContent = playerTurn;
+                return [2, 2];
         }
     }
 
@@ -56,7 +104,7 @@ const displayController = (() => {
     };
 })();
 
-const player = (name) => {
+const Player = (name) => {
     let score = 0;
 
     const increaseScore = () => {
@@ -71,6 +119,12 @@ const player = (name) => {
 
 board.forEach(cell => {
     cell.addEventListener('click', (event) => {
-        displayController.changeDisplay(event);
+        const sym = gameBoard.makeTurn();
+        const cellCoord = displayController.changeDisplay(event, sym);
+        gameBoard.addSym(sym, cellCoord[0], cellCoord[1]);
+        console.log(gameBoard.board);
     })
 });
+
+const michaelangelo = Player('Michaelangelo');
+const diana = Player('Diana');
